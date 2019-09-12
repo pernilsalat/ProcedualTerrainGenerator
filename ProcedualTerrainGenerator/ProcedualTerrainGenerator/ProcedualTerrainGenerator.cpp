e6d397583bac4d9ad667d9eff9185b03fd974400
kirian <jazzel88@gmail.com>
2019-09-12 20:27:34 +0200

    hello world

--- ProcedualTerrainGenerator/ProcedualTerrainGenerator/ProcedualTerrainGenerator.cpp
+++ ProcedualTerrainGenerator/ProcedualTerrainGenerator/ProcedualTerrainGenerator.cpp
@@ -1,13 +1,43 @@
 // ProcedualTerrainGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <GLFW/glfw3.h>
 #include <iostream>
 
-int main()
+int main(void)
 {
-    std::cout << "Hello World!\n";
-}
+	GLFWwindow* window;
 
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
